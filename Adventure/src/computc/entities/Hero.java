8da76e022a7cb21c690acc2500193e03e2e1ba24
jshiraef <joeshiraef@gmail.com>
2014-09-11 03:34:18 -0400

    Chain is now attached to hero; link rotation needs work
    - code needs to be refactored into chain class
    - right now it hangs like a loose appendage
    - gravity needs to be altered
    - room transitions need to be fixed for the chain

--- Adventure/src/computc/entities/Hero.java
+++ Adventure/src/computc/entities/Hero.java
@@ -1,7 +1,17 @@
 package computc.entities;
 
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Set;
 
+import org.jbox2d.collision.shapes.PolygonShape;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.FixtureDef;
+import org.jbox2d.dynamics.World;
+import org.jbox2d.dynamics.joints.RevoluteJointDef;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
@@ -16,6 +26,25 @@ public class Hero extends Entity
 {
 	private boolean dead = false;
 	
+	Image chainLink = new Image("res/links.png");
+	
+	
+	// box2d "world"
+	 private World world;
+	 
+	 private final Vec2 gravity = new Vec2(0, .5f);
+	 
+	 private  Set<Body> bodies = new HashSet<Body>();
+	 
+	  BodyDef playerBodyDef, linkBodyDef;
+	  Body playerBody, linkBody;
+	  PolygonShape playerBoxShape, chainLinkShape;
+	 FixtureDef chainProperties;
+	 RevoluteJointDef joint;
+	 
+	 private float anchorY;
+	 private Vec2 anchor;
+	
 	public Hero(Dungeon dungeon, Room room, int tx, int ty) throws SlickException
 	{
 		super(dungeon, room.getRoomyX(), room.getRoomyY(), tx, ty);
@@ -28,10 +57,70 @@ public class Hero extends Entity
 		this.currentHealth = this.maximumHealth = 3;
 		
 		this.image = new Image("res/hero.png");
+		
+		
+		// box2d stuff (chain)
+		this.world = new World(gravity);
+		
+		playerBody = null;
+		
+		// setup Player's box2d body 
+	 	playerBodyDef = new BodyDef();
+	 	playerBodyDef.type = BodyType.STATIC;
+		playerBody = world.createBody(playerBodyDef);
+		playerBoxShape = new PolygonShape();
+		playerBoxShape.setAsBox(1.6f, 1.6f);
+		playerBody.createFixture(playerBoxShape, 0.0f);
+	
+	
+		chainLinkShape = new PolygonShape();
+		chainLinkShape.setAsBox(0.6f, 0.125f);
+		
+		// setup chain Properties ( FixtureDef)
+		chainProperties = new FixtureDef();
+		chainProperties.shape = chainLinkShape;
+		chainProperties.density = 20.0f;
+		chainProperties.friction = 0.2f;
+					
+		// joint setup
+		joint = new RevoluteJointDef();
+		joint.collideConnected = false;
+		anchorY = this.getRoomPositionY()/30f;
+		Body prevBody = playerBody;
+		
+		// make chain links
+		for (float i = this.getRoomPositionX()/30; i < this.getRoomPositionY()/30 + 15; i++)
+		{
+			linkBodyDef = new BodyDef();
+			linkBodyDef.type = BodyType.DYNAMIC;
+			linkBodyDef.position.set(0.5f + i, anchorY);
+			linkBody = world.createBody(linkBodyDef);	
+			linkBody.createFixture(chainProperties);
+			anchor = new Vec2(i, anchorY);
+			
+			// initialize joint
+			joint.initialize(prevBody, linkBody, anchor);
+			world.createJoint(joint);
+			prevBody = linkBody;
+			bodies.add(linkBody);
+		}
+		
 	}
 	
 	public void render(Graphics graphics, Camera camera)
 	{
+		// draw chain - chain link rotation needs work
+		for(Body body: bodies)
+		{
+			if(body.getType() == BodyType.DYNAMIC) 
+			{
+				Vec2 bodyPosition = body.getPosition().mul(30);
+				chainLink.draw(bodyPosition.x, bodyPosition.y);
+				System.out.println("the link should be drawn at" + bodyPosition.x + " , " + bodyPosition.y);
+				chainLink.setRotation((float) Math.toDegrees(body.getAngle()));
+			}
+		}
+		
 		if(blinking) 
 		{
 			if(blinkCooldown % 4 == 0) 
@@ -39,12 +128,23 @@ public class Hero extends Entity
 				return;
 			}
 		}
+		
+		
 			
 		super.render(graphics, camera);
 	}
 	
 	public void update(Input input, int delta)
 	{
+		// converts box2d position to hero's position on screen
+		Vec2 box2DplayerPosition = new Vec2(this.getRoomPositionX()/30 - 12, this.getRoomPositionY()/30 - 4);
+				
+		// binds the chain to the hero's position
+		playerBody.setTransform(box2DplayerPosition, 0);
+					
+		// the update method for the box2d world
+		world.step(1/60f, 8, 3);
+		
 		getNextPosition(input, delta);
 		checkTileMapCollision();
 		setPosition(xtemp, ytemp);
@@ -72,6 +172,7 @@ public class Hero extends Entity
 		this.dungeon.getRoom(this.getRoomyX(), this.getRoomyY()).visited = true;
 		
 		super.update(delta);
+		
 	}
 	
 	// movement method
